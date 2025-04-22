922df200d1beea4a52591d577b0f9e103e4dbf99
Sk Mijanur Rahaman <sk.mijanur.rahaman@mettletech.in>
2025-04-22 17:43:00 +0530

    Rspec test fail (handle string input)

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  it "returns 0 for empty string" do
+    expect(StringCalculator.new.add("")).to eq(0)
+  end
+end
+
