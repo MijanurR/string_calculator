776d7661ad886b8800cc5b804593a126594da769
Sk Mijanur Rahaman <sk.mijanur.rahaman@mettletech.in>
2025-04-22 21:05:51 +0530

    Support custom delimiter with // syntax

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,14 @@
 class StringCalculator
   def add(input)
      return 0 if input.strip.empty? #handling empty string 
-	   input.split(/,|\n/).map(&:to_i).sum
+	   delimiter = /,|\n/
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
+      custom_delimiter = Regexp.escape(parts[0][2..])
+      delimiter = /#{custom_delimiter}/
+      input = parts[1]
+    end
+
+    input.split(delimiter).map(&:to_i).sum
   end
 end
