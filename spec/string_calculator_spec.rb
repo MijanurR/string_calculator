9873259463fd4a7b68ea275ee8fc8f4de4e0551c
Sk Mijanur Rahaman <sk.mijanur.rahaman@mettletech.in>
2025-04-22 18:11:12 +0530

    Test fails for the sum of numbers separated by newline

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe StringCalculator do
     expect(StringCalculator.new.add("1,2")).to eq(3)
   end
 
-  
+  it "returns the sum of numbers separated by newline" do
+    expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+  end
 end
 
