5a0d07a0bcab6157f151fe2bd0072b8bb3792a48
Sk Mijanur Rahaman <sk.mijanur.rahaman@mettletech.in>
2025-04-22 17:59:59 +0530

    Test fails for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe StringCalculator do
   it "returns 0 for empty string" do
     expect(StringCalculator.new.add("")).to eq(0)
   end
+  
+  it "returns the number itself for a single number input" do
+      expect(StringCalculator.new.add("1")).to eq(1)
+  end
 end
 
