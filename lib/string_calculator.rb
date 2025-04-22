ee71ea982eb875909c4a235027fff17785bf8191
Sk Mijanur Rahaman <sk.mijanur.rahaman@mettletech.in>
2025-04-22 18:12:47 +0530

    Add support for newline as delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,6 @@
 class StringCalculator
   def add(input)
      return 0 if input.strip.empty? #handling empty string 
-	   input.split(",").map(&:to_i).sum
+	   input.split(/,|\n/).map(&:to_i).sum
   end
 end
