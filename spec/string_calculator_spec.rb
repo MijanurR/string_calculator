b5c54783fad16aba037041d1bb9075a34db2d095
Sk Mijanur Rahaman <sk.mijanur.rahaman@mettletech.in>
2025-04-22 18:05:55 +0530

    Test fails for two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe StringCalculator do
   it "returns the number itself for a single number input" do
       expect(StringCalculator.new.add("1")).to eq(1)
   end
+
+
+  it "returns the sum of two comma-separated numbers" do
+    expect(StringCalculator.new.add("1,2")).to eq(3)
+  end
+
+  
 end
 
