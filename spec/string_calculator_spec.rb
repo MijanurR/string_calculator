1986fabceb887c60b440fbdc3a00ce9a309d07ec
Sk Mijanur Rahaman <sk.mijanur.rahaman@mettletech.in>
2025-04-22 21:08:32 +0530

    Test fails to raise exception for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe StringCalculator do
   it "supports custom delimiter defined at the start" do
     expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
   end
-  
+
+  it "raises an exception with message for negative numbers" do
+    expect {
+      StringCalculator.new.add("1,-2,3,-4")
+    }.to raise_error("Negatives not allowed: -2, -4")
+  end
+
 end
 
